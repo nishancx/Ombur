8d3aa0f54ac4c8e0ee8731df53d9276c7b7e71c2
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-05-02 01:00:18 +0545

    Authentication (#2)
    * Add google oauth
    
    * Sign in/out, show state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 
-export default function Home() {
-  return <div className={styles.title}>Ombur</div>;
+export default async function Home() {
+
+  return (
+    <div>.</div>
+  );
 }
