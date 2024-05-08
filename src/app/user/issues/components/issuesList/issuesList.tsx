ec25b97bc32bd2dec3dfdf892246f3fd6ff5aae1
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-05-08 22:12:52 +0545

    Add create issue button

--- src/app/user/issues/components/issuesList/issuesList.tsx
+++ src/app/user/issues/components/issuesList/issuesList.tsx
@@ -2,6 +2,8 @@ import styles from "./issuesList.module.css";
 import { useQuery } from "@tanstack/react-query";
 import { getClientProfileInfo } from "./serverActions";
 import Image from "next/image";
+import { Button } from "@/components";
+import { PlusIcon } from "lucide-react";
 
 type IssuesListProps = {
   setIssueId: (issueId: string) => void;
@@ -16,21 +18,32 @@ const IssuesList: React.FC<IssuesListProps> = ({ setIssueId, clientId }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.clientInfo}>
-        {isLoading ? (
-          <></>
-        ) : (
-          <>
-            <Image
-              src={data.avatar}
-              alt="avatar"
-              width={50}
-              height={50}
-              className={styles.clientAvatar}
-            />
-            <div className={styles.clientName}>{data.name}</div>
-          </>
-        )}
+      <div className={styles.top}>
+        <div className={styles.clientInfo}>
+          {isLoading ? (
+            <></>
+          ) : (
+            <>
+              <Image
+                src={data.avatar}
+                alt="avatar"
+                width={50}
+                height={50}
+                className={styles.clientAvatar}
+              />
+              <div className={styles.clientName}>{data.name}</div>
+            </>
+          )}
+        </div>
+
+        <div className={styles.createIssueContainer}>
+          <Button onClick={() => {}} className={styles.createIssueButton}>
+            <div className={styles.createIssueButtonContent}>
+              <PlusIcon size={24} />
+              <div>Create Issue</div>
+            </div>
+          </Button>
+        </div>
       </div>
     </div>
   );
