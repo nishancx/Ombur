8ab2398c4893ca3f675b8b2425c67b586f28acdf
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-05-08 22:02:22 +0545

    Show client info

--- src/app/user/issues/components/issuesList/issuesList.tsx
+++ src/app/user/issues/components/issuesList/issuesList.tsx
@@ -1,4 +1,7 @@
 import styles from "./issuesList.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { getClientProfileInfo } from "./serverActions";
+import Image from "next/image";
 
 type IssuesListProps = {
   setIssueId: (issueId: string) => void;
@@ -6,9 +9,29 @@ type IssuesListProps = {
 };
 
 const IssuesList: React.FC<IssuesListProps> = ({ setIssueId, clientId }) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["clientProfileInfo", { clientId }],
+    queryFn: () => getClientProfileInfo({ clientId }),
+  });
+
   return (
     <div className={styles.container}>
-      <div className={styles.userInfo}></div>
+      <div className={styles.clientInfo}>
+        {isLoading ? (
+          <></>
+        ) : (
+          <>
+            <Image
+              src={data.avatar}
+              alt="avatar"
+              width={50}
+              height={50}
+              className={styles.clientAvatar}
+            />
+            <div className={styles.clientName}>{data.name}</div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
