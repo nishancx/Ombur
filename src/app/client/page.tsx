6a1b05accdf507b708203c4e23b137086b2c8d7e
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-06-12 21:58:10 +0545

    Remove redirect

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -2,13 +2,11 @@ import { ClientBlock } from "./components/client/client";
 
 import { auth } from "@/../auth";
 
-import { redirect } from "next/navigation";
-
 export default async function Home() {
   const session = await auth();
 
   if (!session) {
-    redirect("/");
+    return null;
   }
 
   return <ClientBlock />;
