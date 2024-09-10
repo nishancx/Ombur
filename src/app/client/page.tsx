7a61d0fbd2beee27077057060e5b5639cded0188
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-09-10 14:27:52 +0545

    Show client page content

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,22 +1,5 @@
 import { ClientPageContent } from "./components";
 
-import { auth } from "@/../auth";
-import { FullSpanLoader } from "@/components/fullSpanLoader/fullSpanLoader";
-import { MESSAGE } from "@/constants/message";
-import { redirect } from "next/navigation";
-
 export default async function Client() {
-  return <div>this is client page</div>;
-  // const session = await auth();
-
-  // if (session?.user?.type === MESSAGE.SENDER_TYPE_INDEX.USER) {
-  //   redirect("/");
-  //   return <FullSpanLoader />;
-  // }
-
-  // if (!session) {
-  //   return null;
-  // }
-
-  // return <ClientPageContent />;
+  return <ClientPageContent />;
 }
