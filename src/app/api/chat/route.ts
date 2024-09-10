1b0b22011f1305bf16c95ec249b2373949715fce
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-09-10 18:18:21 +0545

    Check session manually

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -7,17 +7,30 @@ import { Clients } from "@/libs/server/models/client";
 import { Users } from "@/libs/server/models/user";
 import { Messages } from "@/libs/server/models/message";
 
+import { cookies } from "next/dist/client/components/headers";
+import { decode } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import { connectDB } from "@/libs/server/mongo";
+import { Client } from "@/types/models/client";
 
 var sseIdMap: Map<string, WritableStreamDefaultWriter<any>> = new Map();
 
 export async function GET(req: Request) {
   try {
-    // const session = await auth();
+    const sessionToken = getSessionToken();
+    const decoded = await decode({
+      token: sessionToken?.value,
+      secret: process.env.AUTH_SECRET || "",
+      salt: sessionToken?.name || "",
+    });
 
-    const senderId = "663f8b047f19d7e2a7ed6df6";
-    // const senderId = session?.user?.id;
+    const senderEmail = decoded?.email;
+
+    if (!senderEmail) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const senderId = "session?.user?.id";
 
     if (!senderId) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
@@ -25,27 +38,6 @@ export async function GET(req: Request) {
 
     let responseStream = new TransformStream();
     const writer = responseStream.writable.getWriter();
-
-    const encoder = new TextEncoder();
-
-    setInterval(() => {
-      console.log("write");
-      writer.write(
-        encoder.encode(
-          `data: ${JSON.stringify({
-            _id: Math.random().toString(36).substring(2, 9),
-            issueId: "66e011eb647f2c3f8fe616a3",
-            sender: "user",
-            clientId: "663f8b047f19d7e2a7ed6df6",
-            userId: "66e011c1b69e4aba2477f1ee",
-            text: new Date().getTime().toString(),
-            createdAt: { $date: { $numberLong: "1725873645590" } },
-            updatedAt: { $date: { $numberLong: "1725873645590" } },
-            __v: { $numberInt: "0" },
-          })}\n\n`
-        )
-      );
-    }, 1000);
 
     req.signal.onabort = () => {
       writer.close();
@@ -67,69 +59,86 @@ export async function GET(req: Request) {
 }
 
 export async function POST(request: Request) {
-  return NextResponse.json({}, { status: 200 });
-  // const body = await request.json();
-  // const { text, issueId, userId, clientId, sender } = body;
-  // const isPayloadValid = createMessageValidationSchema.safeParse({
-  //   text,
-  //   issueId,
-  //   userId,
-  //   clientId,
-  //   sender,
-  // });
+  const body = await request.json();
+  const { text, issueId, userId, clientId, sender } = body;
 
-  // if (!isPayloadValid.success) {
-  //   return NextResponse.json({ error: isPayloadValid.error }, { status: 400 });
-  // }
+  const isPayloadValid = createMessageValidationSchema.safeParse({
+    text,
+    issueId,
+    userId,
+    clientId,
+    sender,
+  });
 
-  // await connectDB();
+  if (!isPayloadValid.success) {
+    return NextResponse.json({ error: isPayloadValid.error }, { status: 400 });
+  }
 
-  // const session = await auth();
+  await connectDB();
 
-  // if (!session || !session?.user) {
-  //   throw new Error("Unauthorized");
-  // }
+  const session = await auth();
 
-  // if (sender === MESSAGE.SENDER_TYPE_INDEX.CLIENT) {
-  //   const client = await Clients.findOne({
-  //     _id: session.user.id,
-  //   });
+  if (!session || !session?.user) {
+    throw new Error("Unauthorized");
+  }
 
-  //   if (!client) {
-  //     throw new Error("Client not found");
-  //   }
-  // }
+  if (sender === MESSAGE.SENDER_TYPE_INDEX.CLIENT) {
+    const client = await Clients.findOne({
+      _id: session.user.id,
+    });
 
-  // if (sender === MESSAGE.SENDER_TYPE_INDEX.USER) {
-  //   const user = await Users.findOne({
-  //     _id: session.user.id,
-  //   });
+    if (!client) {
+      throw new Error("Client not found");
+    }
+  }
 
-  //   if (!user) {
-  //     throw new Error("User not found");
-  //   }
-  // }
+  if (sender === MESSAGE.SENDER_TYPE_INDEX.USER) {
+    const user = await Users.findOne({
+      _id: session.user.id,
+    });
 
-  // const newMessage = await Messages.create({
-  //   sender,
-  //   issueId,
-  //   userId,
-  //   clientId,
-  //   text,
-  // });
+    if (!user) {
+      throw new Error("User not found");
+    }
+  }
 
-  // const receiverId =
-  //   sender === MESSAGE.SENDER_TYPE_INDEX.CLIENT ? userId : clientId;
+  const newMessage = await Messages.create({
+    sender,
+    issueId,
+    userId,
+    clientId,
+    text,
+  });
 
-  // const receiverWriter = sseIdMap.get(receiverId);
+  const receiverId =
+    sender === MESSAGE.SENDER_TYPE_INDEX.CLIENT ? userId : clientId;
 
-  // if (receiverWriter) {
-  //   const encoder = new TextEncoder();
+  const receiverWriter = sseIdMap.get(receiverId);
 
-  //   receiverWriter.write(
-  //     encoder.encode(`data: ${JSON.stringify(newMessage)}\n\n`)
-  //   );
-  // }
+  if (receiverWriter) {
+    const encoder = new TextEncoder();
 
-  // return NextResponse.json(newMessage, { status: 200 });
+    receiverWriter.write(
+      encoder.encode(`data: ${JSON.stringify(newMessage)}\n\n`)
+    );
+  }
+
+  return NextResponse.json(newMessage, { status: 200 });
 }
+
+function getSessionToken() {
+  if (process.env.ENVIRONMENT === "development") {
+    // served over HTTP
+    return cookies().get("authjs.session-token");
+  }
+  if (process.env.ENVIRONMENT === "production") {
+    // served over HTTPS
+    return cookies().get("__Secure-authjs.session-token");
+  }
+  return {
+    name: "",
+    value: "",
+  };
+}
+
+const fetchClient = async (payload: Partial<Client>) => {};
