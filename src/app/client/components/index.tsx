05eaadcaf60b3d3b13509cc6f73bfd96af403d09
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-09-10 16:30:00 +0545

    log

--- src/app/client/components/index.tsx
+++ src/app/client/components/index.tsx
@@ -29,39 +29,40 @@ const ClientPageContent: React.FC = () => {
       );
 
       events.onmessage = (rawMessage) => {
-        const message = JSON.parse(rawMessage.data);
+        console.log(rawMessage);
+        // const message = JSON.parse(rawMessage.data);
 
-        const issueId = message?.issueId;
+        // const issueId = message?.issueId;
 
-        if (!issueId) return;
+        // if (!issueId) return;
 
-        const previousMessages:
-          | InfiniteData<
-              {
-                data: Message[];
-                page: number;
-              },
-              number
-            >
-          | undefined = queryClient.getQueryData(
-          QUERY.QUERY_KEYS.GET_CLIENT_CHAT({ issueId })
-        );
+        // const previousMessages:
+        //   | InfiniteData<
+        //       {
+        //         data: Message[];
+        //         page: number;
+        //       },
+        //       number
+        //     >
+        //   | undefined = queryClient.getQueryData(
+        //   QUERY.QUERY_KEYS.GET_CLIENT_CHAT({ issueId })
+        // );
 
-        queryClient.setQueryData(
-          QUERY.QUERY_KEYS.GET_CLIENT_CHAT({ issueId }),
-          {
-            pages: [
-              {
-                data: [message, ...(previousMessages?.pages?.[0]?.data || [])],
-                page: 0,
-              },
-              ...(previousMessages?.pages
-                ? previousMessages.pages.slice(1)
-                : []),
-            ],
-            pageParams: previousMessages?.pageParams,
-          }
-        );
+        // queryClient.setQueryData(
+        //   QUERY.QUERY_KEYS.GET_CLIENT_CHAT({ issueId }),
+        //   {
+        //     pages: [
+        //       {
+        //         data: [message, ...(previousMessages?.pages?.[0]?.data || [])],
+        //         page: 0,
+        //       },
+        //       ...(previousMessages?.pages
+        //         ? previousMessages.pages.slice(1)
+        //         : []),
+        //     ],
+        //     pageParams: previousMessages?.pageParams,
+        //   }
+        // );
       };
 
       setSseStatus("connected");
